975da1c0303617dbecb06b764458f256bffa1704
Unknown <louisponet@gmail.com>
2018-01-09 10:55:39 +0000

    asdf
    adf

--- scripts/all_exchanges.jl
+++ scripts/all_exchanges.jl
@@ -3,7 +3,7 @@ using DFWannier
 qe = load_job("/home/ponet/GdMn2O5/NSOC/qe/")
 hamis = qe.local_dir .* DFControl.search_dir(qe.local_dir, "hr.dat")
 wan_file = qe.local_dir * DFControl.search_dir(qe.local_dir, "wan_up.win")[1]
-fermi = read_fermi_from_qe_file(qe.local_dir * search_dir(qe.local_dir,"scf.out")[1])
+fermi = read_fermi_from_qe_file(qe.local_dir * DFControl.search_dir(qe.local_dir,"scf.out")[1])
 exchanges = WannExchanges(hamis[2], hamis[1], wan_file, fermi)
 for i = 1:length(exchanges.infos)
     for j = i+1:length(exchanges.infos)
