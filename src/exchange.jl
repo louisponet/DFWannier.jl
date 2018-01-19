d9720645264a1023ffcc17d2ddf4de4b38ea2650
Unknown <louisponet@gmail.com>
2018-01-19 17:19:20 +0100

    testre

--- src/exchange.jl
+++ src/exchange.jl
@@ -191,6 +191,7 @@ function WannExchanges(hami_raw_up::Array, hami_raw_dn::Array,  orb_infos::Array
 end
 function WannExchanges(hami_up_file::String, hami_down_file::String, wannier_input_file::String, args...; kwargs...)
     tmp = get_wan_projections(wannier_input_file)
+    println(tmp)
     WannExchanges(read_hami_file(hami_up_file), read_hami_file(hami_down_file), tmp, args...; kwargs...)
 end
 
