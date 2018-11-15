c53a6cb18b04dddf7d0d11cff5984e56fe6c9967
Unknown <louisponet@gmail.com>
2018-11-15 22:23:08 +0100

    f

--- src/fileio.jl
+++ src/fileio.jl
@@ -324,7 +324,7 @@ function write_exchanges(filename::String, structure::Structure)
             for atom2 in structure.atoms[i + 1:end]
                 J = exchange_between(atom1, atom2, exchanges)
                 if J != 0
-                    write(f, "$(getfirst(structure.atoms, atom1)) $(getfirst(structure.atoms, atom2)) $J\n")
+                    write(f, "$(getfirst(x-> x== atom1, structure.atoms)) $(getfirst(x->x==atom2, structure.atoms)) $J\n")
                 end
             end
         end
