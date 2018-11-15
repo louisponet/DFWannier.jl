05c076b1cb762f44e314e4d02ca058865eaaebb1
Unknown <louisponet@gmail.com>
2018-11-15 21:21:39 +0100

    trace

--- src/exchange.jl
+++ src/exchange.jl
@@ -119,7 +119,7 @@ function calcexchanges(hamis,  structure::Structure, fermi::T;
         for (eid, exch) in enumerate(exchanges)
             rm = range(exch.proj1)
             rn = range(exch.proj2)
-            t_js[eid][tid] .+= sign(real(trace(view(D, rm, rm)))) .* sign(real(trace(view(D,rn, rn)))) .* imag(view(D,rm, rm) * view(g[1],rm, rn) * view(D,rn, rn) * view(g[2],rn, rm) * dω)
+            t_js[eid][tid] .+= sign(real(tr(view(D, rm, rm)))) .* sign(real(tr(view(D,rn, rn)))) .* imag(view(D,rm, rm) * view(g[1],rm, rn) * view(D,rn, rn) * view(g[2],rn, rm) * dω)
             # t_js[eid][tid] .+= imag(view(D,rm, rm) * view(g[1],rm, rn) * view(D,rn, rn) * view(g[2],rn, rm) * dω)
         end
     end
@@ -156,7 +156,7 @@ function exchange_between(atom1::Atom{T}, atom2::Atom{T}, exchanges::Array{Excha
     exch = zero(T)
     for e in exchanges
         if e.atom1 == atom1 && e.atom2 == atom2
-            exch += trace(e.J)
+            exch += tr(e.J)
         end
     end
     return exch
