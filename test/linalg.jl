be30bc336dabbbbb320eee43353aa5672704aec6
Unknown <louisponet@gmail.com>
2019-05-25 15:19:57 +0200

    tests for blockbandedmatrices diagonalization

--- test/linalg.jl
+++ test/linalg.jl
@@ -1,3 +1,4 @@
+
 for F in (Float32, Float64)
 	t = rand(Complex{F}, 50, 50)
 	orig       = (t + t')/2
@@ -6,9 +7,16 @@ for F in (Float32, Float64)
 	cached_eig = eigen(orig, cache)
 	@test sum(normal_eig.values) ≈ sum(cached_eig.values) 
 	@test Array(normal_eig) ≈ Array(cached_eig) ≈ orig
+end
 
-	tvals = zeros(F, 50)
-	torig = copy(orig)
-	@test Array(eigen!(tvals, torig, cache)) ≈ orig
+for F in (Float32, Float64)
+	t = rand(Complex{F}, 50, 50)
+	orig       = DFW.BlockBandedMatrix((t + t')/2, ([25, 25], [25, 25]), (0, 0))
+	normal_eig1, normal_eig2 = eigen(orig[DFW.Block(1,1)]), eigen(orig[DFW.Block(2,2)])
+	cache      = DFW.EigCache(orig)
+	cached_eig = eigen(orig, cache)
+	@test sum(normal_eig1.values) + sum(normal_eig2.values) ≈ sum(cached_eig.values)
+	@test Array(normal_eig1) ≈ Array(cached_eig)[1:25, 1:25] ≈ orig[DFW.Block(1, 1)]
+	@test Array(normal_eig2) ≈ Array(cached_eig)[26:50, 26:50] ≈ orig[DFW.Block(2, 2)]
 end
 
